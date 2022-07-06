d88fa229559ce78cecd2e742992a988b76756d5d
Kevin Stayner <kstayslc@icloud.com>
2022-07-06 14:48:42 -0600

    Cleaned up the index file

--- index.js
+++ index.js
@@ -1,63 +1,61 @@
 import inquirer from 'inquirer';
 console.log('Testing')
-// import fs from 'fs';
-// const fs = require('fs/promises');
+
 import fs from 'fs/promises'
 import { generateLicenseBadge } from './utility/markdown.js';
 
 inquirer
   .prompt([
-    /* Pass your questions in here */
     {
         type: "input",
         message: "What is the name of your project?",
         name: 'name',
     }, 
-//     {
-//       type: "input",
-//       message: "What is your GitHub username",
-//       name: 'GitHub',
-//   }, 
-//   {
-//       type: "input",
-//       message: "What is the title of your repository?",
-//       name: 'title',
-//   },  
-//   {
-//       type: "input",
-//       message: "Please provide a table of contents:",
-//       name: 'tableofcontents',
-//   }, 
-//   {
-//       type: "input",
-//       message: "What is the purpose of this project?",
-//       name: 'purpose',
-//   }, 
-//   {
-//       type: "input",
-//       message: "Deployment link/URL:",
-//       name: 'livelink'
-//   }, 
-//   {
-//       type: "editor",
-//       message: "Instalation instructions:",
-//       name: 'instalation'
-//   }, 
-//   {
-//       type: "input",
-//       message: "Contributors: ",
-//       name: 'contributors',
-//   }, 
-//   {
-//       type: "input",
-//       message: "Tests:",
-//       name: 'tests',
-//   }, 
-//   {
-//       type: "editor",
-//       message: "Questions: (this section will include GitHub information)",
-//       name: 'questions',
-//   }, 
+    {
+      type: "input",
+      message: "What is your GitHub username",
+      name: 'GitHub',
+  }, 
+  {
+      type: "input",
+      message: "What is the title of your repository?",
+      name: 'title',
+  },  
+  {
+      type: "input",
+      message: "Please provide a table of contents:",
+      name: 'tableofcontents',
+  }, 
+  {
+      type: "input",
+      message: "What is the purpose of this project?",
+      name: 'purpose',
+  }, 
+  {
+      type: "input",
+      message: "Deployment link/URL:",
+      name: 'livelink'
+  }, 
+  {
+      type: "editor",
+      message: "Instalation instructions:",
+      name: 'instalation'
+  }, 
+  {
+      type: "input",
+      message: "Contributors: ",
+      name: 'contributors',
+  }, 
+  {
+      type: "input",
+      message: "Tests:",
+      name: 'tests',
+  }, 
+  {
+      type: "editor",
+      message: "Questions: (this section will include GitHub information)",
+      name: 'questions',
+  }, 
   {
       type: 'list',
       message: "This project is licensed with:",
@@ -76,10 +74,7 @@ inquirer
 ## ${response.tableofcontents}
 * [ Description ](#about)
 * [ Installation ](#installation)
-* [ Usage ](#usage)
 * [ License ](#license)
-* [ Badges ](#badges)
-* [ Test Instructions ](#test)
 * [ Contributing ](#contribution)
 * [ Questions ](#questions)
   
@@ -87,24 +82,24 @@ inquirer
 ## Description
     ${response.purpose}
   
-    <a name="installation"></a>
+   
 ## Installation
     ${response.instalation}
   
   
-    <a name="contribution"></a>
+   
 ## Contributing
-    ${response.contributors}
+    ${response.contribution}
   
-    <a name="test"></a>
+  
 ## Tests
     ${response.tests}
   
-    <a name="license"></a>
+   
 ## License
     ${response.licenses} 
 *${generateLicenseBadge(`${response.licenses}`)}
-    <a name="questions"></a>
+   
 ## Questions
     ${response.questions}
 * [Find me on GitHub](https://github.com/${response.Github}/)
@@ -113,7 +108,6 @@ inquirer
     `; 
     createFile(generateDoc);
 });
-  //   // Use user feedback for... whatever!!
 
 
 async function createFile(generateDoc){
@@ -124,28 +118,4 @@ async function createFile(generateDoc){
     } catch (err) {
         console.log('err', err);
     }
-}
-
-    // fs.writeFile("README.md", answers, (err) => {
-    //   if(err) {console.log(err)}    
-    //   console.log('readme created')
-    // })
-  
-  // .catch((error) => {
-  //   if (error.isTtyError){
-  //     console.log('ERROR: ', error)
-  //   } else {
-  //     console.log('Something Else went Wrong');
-  //   }
-    
-  // }
-// From Kasey
-  //Creates Badge with link for license
-// const renderLicenseBadge = (license) => {
-//     const badges = {
-//       mit: `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
-//       isc: `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`,
-//       gnuplv3: `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
-//     }
-//     return badges[license]
-//   }
+};
