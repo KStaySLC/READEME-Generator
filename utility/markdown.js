590fc9a232a19a7f3f1cae41b6789907201c4e5b
Kevin Stayner <kstayslc@icloud.com>
2022-07-06 14:50:40 -0600

    Cleaned up unnecessary code in markdown.js

--- utility/markdown.js
+++ utility/markdown.js
@@ -1,8 +1,4 @@
-// const fs = require('fs');
-
 let selectedLicense;
-// let selectedLicenseInfo;
-// let selectedLicenseLink;
 
 export function generateLicenseBadge(license) {
     if (license === "Apache 2.0") {
@@ -13,56 +9,4 @@ export function generateLicenseBadge(license) {
         selectedLicense = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg'
     }
     return selectedLicense;
-}
-
-//['Apache 2.0', 'GNU (GPL)', 'MIT', 'Mozilla Public License 2.0', 'No license']
-
-// function generateMarkdown(data) {
-//     const licenseBadge = generateLicenseBadge(`${data.license}`);
-
-//     return `
-//     # ${data.title} 
-
-//     ## Table of Contents
-//     * [ Description ](#about)
-//     * [ Installation ](#installation)
-//     * [ Usage ](#usage)
-//     * [ License ](#license)
-//     * [ Badges ](#badges)
-//     * [ Test Instructions ](#test)
-//     * [ Contributing ](#contribution)
-//     * [ Questions ](#questions)
-  
-//     <a name="about"></a>
-//     ## Description
-//     ${data.about}
-  
-//     <a name="installation"></a>
-//     ## Installation
-//     ${data.instructions}
-  
-//     <a name="usage"></a>
-//     ## Usage
-//     ${data.usage}
-  
-//     <a name="contribution"></a>
-//     ## Contributing
-//     ${data.contribution}
-  
-//     <a name="test"></a>
-//     ## Tests
-//     ${data.test}
-  
-//     <a name="license"></a>
-//     ## License
-//     ${data.license} - ${licenseInfo} (${licenseLink}) ${licenseBadge}
-  
-//     <a name="questions"></a>
-//     ## Questions
-//     Feel free to reach out to me with any additional questions
-//     * [Find me on GitHub](https://github.com/${data.github}/)
-//     * Email me at: ${data.email}
-//     `
-// }
-
-// module.exports = generateMarkdown
+};
